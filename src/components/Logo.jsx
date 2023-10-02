caef7b94364330dcf17198bf206421d45e2ad8a9
Maksim Kalinin <kalinin.m.a@bk.ru>
2023-10-02 18:55:47 +0300

    Add home link

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,13 @@
+import { Link } from 'react-router-dom';
+
 import styles from './Logo.module.css';
 
 const Logo = () => {
-  return <img className={styles.logo} src="/logo.png" alt="WorldWise logo" />;
+  return (
+    <Link to="/">
+      <img className={styles.logo} src="/logo.png" alt="WorldWise logo" />
+    </Link>
+  );
 };
 
 export default Logo;
